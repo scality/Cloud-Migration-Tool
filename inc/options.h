5e45ab2a0564cf5b804a56fe8e498b6314e2b267
David Pineau <dav.pineau@gmail.com>
2011-01-14 22:07:00 +0100

    First commit with basic CMakeLists.txt
    Add of .gitignore file ignoring vim backup files and build directory
    Add of a basic CMakeLists.txt that configures an include, and compiles
    the code
    Add of almost empty includes and source files.

---
+++ inc/options.h
@@ -0,0 +1,10 @@
+#ifndef __SD_CLOUMIG_OPT_H__
+#define __SD_CLOUMIG_OPT_H__
+
+typedef struct s_cloudmig_options
+{
+	const char* src_profile;
+	const char* dest_profile;
+} t_opt;
+
+#endif /* ! __SD_CLOUMIG_OPT_H__ */
---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+#include "cloudmig.h"
+#include "options.h"
+
+t_opt *	gl_options = NULL;
+
+int main()
+{
+	t_opt options;
+	gl_options = &options;
+	
+	return (EXIT_SUCCESS);
+}
